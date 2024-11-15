e0da620869a3abb0a25f46aeaeb84e14dc46d187
jtb00 <jack.bedinger@msbci.com>
2024-11-15 13:27:42 -0500

    Added function to replace nulls with mean/median/mode

--- final_project.py
+++ final_project.py
@@ -1,7 +1,7 @@
 import numpy as np
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, mean, median, mode, max
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import StringIndexer
 
@@ -18,10 +18,17 @@ def null_features(df):
         percent = float(count) / num_rows
         percents.append(percent)
         # print(feature + '\t' + str(count) + '\t' + str(round(percent, 2)))
+    null_features = []
     for i in range(len(features)):
         if percents[i] >= 0.01:
             df = df.drop(features[i])
-    return df
+        elif percents[i] != 0.0:
+            # Allows for performing operations on specifically these columns later
+            if df.dtypes[df.columns.index(features[i])][1] == 'string':
+                null_features.append(features[i] + '_INDEXED')
+            else:
+                null_features.append(features[i])
+    return df, null_features
 
 
 # Use StringIndexer to map any categorical string columns to a set of integer values
@@ -31,7 +38,7 @@ def index_string_cols(df):
     indexers = []
     for feature in features:
         if df.dtypes[df.columns.index(feature)][1] == 'string':
-            si = StringIndexer(inputCol=feature, outputCol=feature + '_INDEXED')
+            si = StringIndexer(inputCol=feature, outputCol=feature + '_INDEXED', handleInvalid='keep')
             indexers.append(si)
             to_drop.append(feature)
     pipeline = Pipeline(stages=indexers)
@@ -39,6 +46,34 @@ def index_string_cols(df):
     df = df.drop(*to_drop)
     for feature in to_drop:
         df = df.withColumn(feature + '_INDEXED', df[feature + '_INDEXED'].cast('int'))
+        # df = df.withColumnRenamed(feature + '_INDEXED', feature)
+    return df
+
+
+def fill_nulls(df, nulls, int_mode, double_mode):
+    fill = {}
+    for feature in nulls:
+        # Workaround for indexed string columns; because StringIndexer has no option to preserve null values, these were
+        # set equal to the number of different labels. This replaces all rows with this value with null.
+        if '_INDEXED' in feature:
+            max_val = df.select(max(feature)).collect()[0][0]
+            df = df.na.replace(max_val, None, feature)
+        if df.dtypes[df.columns.index(feature)][1] == 'integer':
+            if int_mode == 'median':
+                val = df.agg(median(feature)).collect()[0][0]
+            elif int_mode == 'mode':
+                val = df.agg(mode(feature)).collect()[0][0]
+            else:
+                val = df.agg(mean(feature)).collect()[0][0]
+        else:
+            if double_mode == 'median':
+                val = df.agg(median(feature)).collect()[0][0]
+            elif double_mode == 'mode':
+                val = df.agg(mode(feature)).collect()[0][0]
+            else:
+                val = df.agg(mean(feature)).collect()[0][0]
+        fill[feature] = val
+    df = df.na.fill(fill)
     return df
 
 
@@ -57,26 +92,29 @@ credit_card_balance = spark.read.csv(input_path + '/credit_card_balance.csv', he
 installments_payments = spark.read.csv(input_path + '/installments_payments.csv', header=True, inferSchema=True)
 POS_CASH_balance = spark.read.csv(input_path + '/POS_CASH_balance.csv', header=True, inferSchema=True)
 previous_application = spark.read.csv(input_path + '/previous_application.csv', header=True, inferSchema=True)
-# print(installments_payments.count())
+# print(previous_application.first())
 
 # Remove any features with > 1% null values
-application = null_features(application)
+application, application_nulls = null_features(application)
 # print(application_nulls)
-bureau = null_features(bureau)
-bureau_balance = null_features(bureau_balance)
-credit_card_balance = null_features(credit_card_balance)
-installments_payments = null_features(installments_payments)
-POS_CASH_balance = null_features(POS_CASH_balance)
-previous_application = null_features(previous_application)
+bureau, bureau_nulls = null_features(bureau)
+bureau_balance, bureau_balance_nulls = null_features(bureau_balance)
+credit_card_balance, credit_card_balance_nulls = null_features(credit_card_balance)
+installments_payments, installments_payments_nulls = null_features(installments_payments)
+POS_CASH_balance, POS_CASH_balance_nulls = null_features(POS_CASH_balance)
+previous_application, previous_application_nulls = null_features(previous_application)
+# print(previous_application.first())
 
 application = index_string_cols(application)
 bureau = index_string_cols(bureau)
 bureau_balance = index_string_cols(bureau_balance)
 credit_card_balance = index_string_cols(credit_card_balance)
 previous_application = index_string_cols(previous_application)
-previous_application.printSchema()
+# print(previous_application.first())
 
 # Handle remaining missing values
+application = fill_nulls(application, application_nulls, 'mode', 'mean')
+print(application.filter(application[application_nulls[0]].isNull()).count())
 
 # Feature engineering
 
